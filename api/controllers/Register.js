ceeef85b32bcf8555880cf60718c21cef1e4983e
Serkan Ozaydin <serkan.zaydn@gmail.com>
2018-03-28 01:19:04 +0300

    added category for events

--- api/controllers/Register.js
+++ api/controllers/Register.js
@@ -2,19 +2,45 @@ var kullanici=require('../mocks/models/User')
 var jsonCreator= require('../helpers/JsonCreator')
 var Constants=require('../helpers/Constants')
 var etkinlik= require('../mocks/models/Event')
+//var category=require('../mocks/models/Category')
 
 
-module.exports = {registerUser};
+module.exports = {registerUser,getEventsCategories};
+//module.exports = {getEventsCategories};
+
 
 
 function getEventsCategories(req, res, next){
+    res.json([{
+           
+        "cId" : "12",
+            "cName" : "üsame"
     
+      },{
+           
+        "cId" : "13",
+            "cName" : "enes"
+    
+      }])
+     /* category.find(function(err,results){
+        if(err) {
+            jsonCreator.commonResponseCreator(Constants.ERROR_CODE,Constants.ERROR_MESSAGE,function(callback){
+                res.status(callback.code)
+                res.send(callback)
+            });
+        }else{
+            jsonCreator.commonResponseCreator(Constants.OK_CODE,Constants.OK_MESSAGE,function(callback){
+                console.log(results);
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(results));
+            });
+        }
+      })*/
 }
 
 
 function registerUser(req, res, next) {
     var udid= req.headers['udid']
-    console.log(udid)
     var user= new kullanici({
         name: req.body['name'],
         udid: req.headers['udid'],
@@ -28,12 +54,7 @@ function registerUser(req, res, next) {
     })
 
     user.save(function(err,user) {
-        if (err) {
-            jsonCreator.commonResponseCreator(Constants.ERROR_CODE,Constants.ERROR_MESSAGE,function(callback){
-                res.status(callback.code)
-                res.send(callback)
-            });
-        }
+        if (err) throw err;
         jsonCreator.registerResponseCreator(Constants.OK_CODE,Constants.OK_MESSAGE,user._id,function(callback){
             res.status(callback.code)
             res.send(callback)
