39a854caac5469f5719ebd0f9cc6a7969191f78e
hsmnzaydn <serkan.zaydn@gmail.com>
2018-05-10 03:28:36 +0300

    Added update profile service

--- api/controllers/ProfileService.js
+++ api/controllers/ProfileService.js
@@ -4,7 +4,7 @@ var Constants=require('../helpers/Constants')
 var Utils=require('../helpers/Utils')
 
 
-module.exports={getProfile}
+module.exports={getProfile,updateProfile}
 
 function getProfile(req,res,next){
 
@@ -44,5 +44,50 @@ function getProfile(req,res,next){
         }
 
     })
+
+}
+
+
+function updateProfile(req,res,next){
+    var udid= req.headers['udid']
+    var authorizationKeyOfUser= req.headers['authorization']
+    var profileId=req.swagger.params.profileId.value
+    var interesting=req.body['interests']
+
+
+    UserSchema.findOne({udid:udid,_id:authorizationKeyOfUser},function(err,user){
+        if(err){
+            console.log(err)
+            jsonCreator.commonResponseCreator(Constants.ERROR_CODE,Constants.ERROR_MESSAGE,function(callback){
+                res.status(callback.code)
+                res.send(callback)
+            })
+        }else{
+            if(user){
+                user.interesting=interesting
+                user.save(function(err){
+                    if(err){
+                        console.log(err)
+                        jsonCreator.commonResponseCreator(Constants.ERROR_CODE,Constants.ERROR_MESSAGE,function(callback){
+                            res.status(callback.code)
+                            res.send(callback)
+                        })
+                    }else{
+                        jsonCreator.commonResponseCreator(Constants.OK_CODE,Constants.OK_MESSAGE,function(callback){
+                            res.status(callback.code)
+                            res.send(callback)
+                        })
+                    }
+                
+                
+                })
+            }
+        }
+
+
+
+    })
+
+
 
 }
